ec070ec16b9d49f24648638c12f99df98bd1fc99
GoingCrazyDude <tezosbiswa@gmail.com>
2024-07-01 20:58:41 +0530

    The Strongest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,49 @@
 <script>
-    import Yandhi from "$lib/assets/yandhi.gif"
+    // import Yandhi from "$lib/assets/yandhi.gif"
+    import Gojo from "$lib/assets/The Strongest.mp4"
 </script>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400&display=swap');
     * {
-        background: #FCFAFC;
+        background: #000000;
         font-family: "JetBrains Mono", monospace;
         font-size: 20px;
         margin: 0;
         padding: 0;
-        left: 50%;
-        top: 50%;
     }
 
-    img {
+    .video {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%; 
+        overflow: hidden;
+    }
+
+    .video video {
+         /* Make video to at least 100% wide and tall */
+        min-width: 100%; 
+        min-height: 100%; 
+
+        /* Setting width & height to auto prevents the browser from stretching or squishing the video */
+        width: auto;
+        height: auto;
+
+        /* Center the video */
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%)
+    }
+
+    /* img {
         position: absolute;
         left: 92.5%;
         top: 85%;
         transform: translate(-50%, -50%);
-    }
+    } */
     
     .bio {
         box-shadow: 5px 5px 2.5px black;
@@ -34,6 +58,7 @@
         left: 50%;
         top: 40%;
         transform: translate(-50%, -50%);
+        z-index: 1;
     }
     a {
         background: #818181;
@@ -43,7 +68,10 @@
 </style>
 
 <body>
-    <img src={Yandhi} alt="Yandhi" width="300" />
+    <div class="video">
+        <video src="{Gojo}" autoplay loop muted></video>
+    </div>
+    <!-- <img src={Yandhi} alt="Yandhi" width="300" /> -->
     <div class="bio">
         hi, im tezos <br>
         idiot programmer, still learning <br>
