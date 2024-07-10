f9b2fac373018b7e9fc3022aee79665006a20320
GoingCrazyDude <tezosbiswa@gmail.com>
2024-07-10 16:34:59 +0530

    Navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<body>
+    <nav class="fixed w-full z-20 top-0 start-0 shadow-lg bg-transparent z-1 backdrop-blur-xl p-1 rounded-md text-white">
+        <div class="max-w-screen-xl flex flex-wrap items-center bg-transparent justify-between mx-auto p-4">
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul class="flex flex-col p-4 md:p-0 mt-4 transparency-fix font-medium rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+                    <li>
+                        <a href="/links" class="block py-2 px-3 hover:text-sky-300">Links</a>
+                    </li>
+                    <li>
+                        <a href="/projects" class="block py-2 px-3 hover:text-sky-300">Projects</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
