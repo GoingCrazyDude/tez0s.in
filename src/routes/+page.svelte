52a480658211ef4075c55b203619054e055941ab
GoingCrazyDude <tezosbiswa@gmail.com>
2024-10-15 20:35:57 +0530

    EACDialog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import wip from "$lib/assets/wip.png"
+    import eac from "$lib/assets/EACLogo.mp4"
 </script>
 <div class="flex justify-center">
     <img class="w-1/2" src="{wip}" alt="WORK IN PROGRESS">
 </div>
+<video src="{eac}"/>
 
