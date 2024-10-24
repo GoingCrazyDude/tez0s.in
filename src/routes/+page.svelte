6a4c734ece10079b8f7e0725257a0e9d08f4518d
GoingCrazyDude <tezosbiswa@gmail.com>
2024-10-24 16:07:11 +0530

    Synapse X logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
     import wip from "$lib/assets/wip.png"
     import eac from "$lib/assets/EACLogo.mp4"
+    import synapsex from "$lib/assets/SynapseXLogo.png"
 </script>
 <div class="flex justify-center">
     <img class="w-1/2" src="{wip}" alt="WORK IN PROGRESS">
 </div>
 <video src="{eac}"/>
-
+<img src="{synapsex}">
