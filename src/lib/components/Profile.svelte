fdf455ccabb2c89f3b79cf58701e21e0a1e10c8f
GoingCrazyDude <tezosbiswa@gmail.com>
2024-11-09 01:13:16 +0530

    Mama! IM DONE!

---
+++ src/lib/components/Profile.svelte
@@ -0,0 +1,96 @@
+<script>
+    import Draggable from "$lib/components/Draggable.svelte";
+    import Question from "../../../static/img/Question.png"
+    export let username;
+    export let pronouns;
+    export let description;
+    export let url;
+    let isVisible = true;
+
+    function toggleVisibility() {
+        isVisible = !isVisible;
+    }
+</script>
+
+<style>
+
+    img {
+        font-weight: 300;
+        line-height: 2;
+        text-align: center;
+
+        display: block;
+        position: relative;
+    }
+
+    img:before {
+        content: "We're sorry but there is no image :(";
+        display: block;
+        margin-bottom: 10px;
+    }
+
+    .window {
+        width: 768px;
+        height: 432px;
+    }
+
+    .titlebar {
+        width: 768px;
+        height: 42px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-left: 12px;
+        padding-right: 12px;
+        background-color: #2f2f2f;
+        border-radius: 8px 8px 0 0;
+    }
+
+    .traffic-light {
+        display: flex;
+        gap: 8px;
+    }
+
+    .light {
+        width: 14px;
+        height: 14px;
+        border-radius: 50%;
+        transition: background-color 0.3s ease;
+    }
+
+    .neo {
+        user-drag: none;
+        -webkit-user-drag: none;
+        user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+    }
+
+    .light:hover {
+        opacity: 0.8;
+    }
+
+</style>
+
+{#if isVisible}
+    <Draggable>
+        <div class="titlebar rounded-md bg-neutral-700 text-white z-10 flex items-center justify-between">
+            <div class="traffic-light">
+                <div class="light light-red bg-red-400"></div>
+                <div class="light light-yellow bg-amber-400"></div>
+                <div class="light light-green bg-green-500"></div>
+            </div>
+        </div>
+        <div class="window rounded-md bg-stone-900 text-white grid drop-shadow-2xl">
+            <div class="grid grid-cols-2">
+                <img src={url} class="neo pt-8 pl-8" alt="profile" width="300" height="300">
+                <p class="text-white pt-8">
+                    Username: {username} <br>
+                    Pronouns: {pronouns} <br>
+                    Description: {description} <br>
+                </p>
+            </div>
+        </div>
+    </Draggable>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,82 @@
-<style>
-    * {
-    }
-</style>
-
 <script>
-    import Window from "$lib/components/Window.svelte";
+    import Profile from "$lib/components/Profile.svelte";
     let username;
+    let pronouns;
+    let description;
+    let url;
+
+    function setProfile(profileName) {
+        switch (profileName) {
+            case 'Skylan':
+                username = 'Skylan';
+                pronouns = 'He/Him/Any';
+                description = 'meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow :3 meow'
+                url = "https://raw.githubusercontent.com/GoingCrazyDude/tez0s.in/refs/heads/main/src/lib/assets/Aero.png";
+                break;
+            case 'Xaur':
+                username = '???';
+                pronouns = '???';
+                description = '???';
+                url = "???";
+                break;
+            case 'Tezos':
+                username = 'Tezos';
+                pronouns = 'They/Them';
+                description = 'Hi! I code and stuff.';
+                url = "https://raw.githubusercontent.com/GoingCrazyDude/tez0s.in/refs/heads/main/src/lib/assets/Yeezus.png";
+                break;
+            default:
+                username = 'none';
+                pronouns = 'none';
+                description = 'none';
+        }
+    }
+
 </script>
+
+<style>
+
+    .background {
+        user-drag: none;
+        -webkit-user-drag: none;
+        user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        background-image: url("/img/Background.png");
+    }
+
+    .dock {
+        position: absolute;
+        transform: translate(-50%, 50%);
+        top: 65%;
+        left: 50%;
+        width: 600px;
+    }
+
+    .profile {
+        user-drag: none;
+        -webkit-user-drag: none;
+        user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+    }
+
+</style>
+<Profile username={username} pronouns={pronouns} description={description} url={url} />
+<div class="background w-full h-screen overflow-hidden"/>
+<div class="dock grid grid-cols-3 rounded-lg backdrop-blur-xl h-44 bg-opacity-10 bg-black">
+    <button class="group perspective-1000" on:click={() => setProfile('Skylan')} >
+        <img class="profile rounded-2xl group-hover:rotate-3d group-hover:scale-105 transition-all duration-300 mx-auto" src="/img/Skylan.png" width="84" height="84" alt="Skylan">
+        <p class="text-3xl text-white text-center">Skylan</p>
+    </button>
+    <button class="group perspective-1000" on:click={() => setProfile('Xaur')} >
+        <img class="profile rounded-2xl group-hover:rotate-3d group-hover:scale-105 transition-all duration-300 mx-auto" src="/img/Xaur.png" width="84" height="84" alt="Xaur">
+        <p class="text-3xl text-white text-center">Xaurchark</p>
+    </button>
+    <button class="group perspective-1000" on:click={() => setProfile('Tezos')}>
+        <img class="profile rounded-2xl group-hover:rotate-3d group-hover:scale-105 transition-all duration-300 mx-auto" src="/img/Tezos.png" width="84" height="84" alt="Tezos">
+        <p class="text-3xl text-white text-center">Tezos</p>
+    </button>
+</div>
