e2eacee19d0f1199145320eefe20da6d469b1f66
GoingCrazyDude <tezosbiswa@gmail.com>
2024-11-10 16:20:01 +0530

    wave users beware

--- src/routes/fixroblox/+page.svelte
+++ src/routes/fixroblox/+page.svelte
@@ -1,6 +1,20 @@
 <style>
+    body {
+        background-color: black;
+        color: white; /* Optional: Makes text white so it's readable */
+        margin: 0; /* Optional: Removes default margin */
+        height: 100vh; /* Optional: Ensures the body takes full viewport height */
+    }
 </style>
 
-<video class="mx-auto bg-black pt-14" height="1200" width="1200" src="/video/scare.mp4" autoplay>
+<body>
+<p class="text-3xl text-white text-center pt-24 pb-24 pl-48 pr-48">
+    Is your Roblox not working?
+    Are you using Wave and or Solara?
+    Here is how to fix your issue!
+    Click play on the video below to see how to fix this issue! (Please turn your volume up to hear the audio)
+</p>
+<video class="mx-auto pt-14" height="500" width="500" src="/video/jumpscare.mp4" controls>
 
 </video>
+</body>
